513aeed71c31fec1952648516c9f81d791b55434
Alex Rosso <alexrosso1025@gmail.com>
2024-06-25 18:47:05 -0500

    If polygon

--- 5. polygonArea.js
+++ 5. polygonArea.js
@@ -1,31 +1,9 @@
-class Polygon {
-    calculateArea(){
-        
+const calculatePolygon = (type, side1, side2) => {
+    if(type === "Triangle"){
+        return (side1 * side2) / 2
+    } else if(type === "Square" || type === "Rectangle"){
+        return side1 * side2
     }
 }
 
-class Triangle extends Polygon{
-    calculateArea(base, height) {
-        return (base*height)/2
-    }   
-}
-
-class Square extends Polygon {
-    calculateArea(side) {
-        return (side*side)
-    }
-}
-
-class Rectangle extends Polygon {
-    calculateArea(side1, side2) {
-        return side1*side2
-    }
-}
-
-const triangle = new Triangle();
-const square = new Square();
-const rectangle = new Rectangle();
-
-triangle.calculateArea(2,4);
-square.calculateArea(2);
-rectangle.calculateArea(3,6);
+calculatePolygon("Triangle", 2, 4)
